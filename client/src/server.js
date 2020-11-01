0cfb23f33fd9f05154bd0b011cc44ef5135e8b48
munhunger <marcusmunger@gmail.com>
2020-11-01 16:37:49 +0100

    use window location

--- client/src/server.js
+++ client/src/server.js
@@ -1,9 +1,14 @@
 import { SubscriptionClient } from "subscriptions-transport-ws";
 import { WebSocketLink } from "apollo-link-ws";
 
-export const wsClient = new SubscriptionClient("ws://localhost:5001/graphql", {
-  reconnect: true,
-});
+console.log(window.location);
+
+export const wsClient = new SubscriptionClient(
+  `ws://${window.location.hostname}:5001/graphql`,
+  {
+    reconnect: true,
+  }
+);
 
 export const client = new WebSocketLink(wsClient);
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { Server } from "./server";
 import { Config, ConfigScanner, Repo } from "./config";
 import { ServiceConfig } from "./services/service";
+import express from "express";
 
 let configuration = Config.load();
 configuration.scan();
@@ -11,4 +12,12 @@ const { monitoring } = require("otto-lib");
 
 monitoring.server(4000).then((monitoring: any) => {
   new Server().startBackend(5001).then(monitoring.setUp());
+
+  let proxy = express();
+
+  proxy.get("/", (req, res) => {
+    res.send("hello world");
+  });
+
+  proxy.listen(80, () => console.log("proxy listening on port 80"));
 });
